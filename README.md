4d196b2f25774452bc275829e6e9e42954e37f07
alessonhu <alessonhu@tencent.com>
2022-03-29 09:48:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+#Go编程模式实践
+
+###1、切片、接口、性能
+base.go
+###2、错误处理
+error.go
+###3、修饰器模式
+###4、委托和控制反转
+###5、Map-Reduce
+###6、Go Generation
+###7、Pipeline
+###8、Visitor
+###9、Functional Option
+functional_option.go
+
+适用场景：不定参数的对象初始化
+
+***
+参考：
+https://coolshell.cn
+
---
+++ functional_option.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//Addr,Port为必须参数，而其他为可选参数
+type Server struct {
+	Addr     string
+	Port     int
+	Protocol string
+	MaxConns int
+	Timeout  time.Duration
+}
+
+//函数类型
+type Option func(*Server)
+
+func Protocol(p string) Option {
+	return func(server *Server) {
+		server.Protocol = p
+	}
+}
+
+func Maxconns(maxconns int) Option {
+	return func(server *Server) {
+		server.MaxConns = maxconns
+	}
+}
+
+func Timeout(timeout time.Duration) Option {
+	return func(server *Server) {
+		server.Timeout = timeout
+	}
+}
+
+//server初始化
+func NewServer(addr string, port int, option ...Option) (*Server, error) {
+	svr := Server{
+		Addr:     addr,
+		Port:     port,
+		Protocol: "tcp",
+		MaxConns: 100,
+		Timeout:  10 * time.Second,
+	}
+	for _, opt := range option {
+		opt(&svr)
+	}
+	return &svr, nil
+}
+
+func main() {
+	s1, _ := NewServer("127.0.0.1", 9443)
+	s2, _ := NewServer("127.0.0.1", 9443, Protocol("udp"))
+	s3, _ := NewServer("127.0.0.1", 944, Protocol("tcp"), Maxconns(1000), Timeout(1*time.Second))
+	fmt.Println(s1, s2, s3)
+}
